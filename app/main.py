02de86433bced178c6461d9f5d90c43d15139b7e
sujanay <sujanay@hotmail.com>
2019-01-22 18:18:55 -0500

    changed in branch

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
-from flask import Flask, redirect, render_template, request, session, url_for
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+def hello_world():
+    return 'Hello, World!'
