24f1183438fa8ae9e53e214c0412dbc36aed175f
sujanay <sujanay@hotmail.com>
2019-01-22 22:03:53 -0500

    working code with template rendering

--- app/main.py
+++ app/main.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Sujanay!'
-    # return render_template('index.html')
-    
+    # renders the template 'index.html' from "templates" folder
+    return render_template('index.html')
+
+@app.route('/<string:name>')
+def hello(name):
+    return render_template('index.html', name=name)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
